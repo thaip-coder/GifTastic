5d908ac61c0ccbfebb80a352856204da1fa0129f
Thai Phan <thaiphan90@yahoo.com>
2019-07-02 09:48:21 -0500

    javascript work

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,4 +1,19 @@
 /* ---------- Variables ---------- */
 var animals = ['horse', 'dog', 'cat', 'wolf', 'giraffe', 'whale', 'tiger', 'elephant', 'rabbit', 'monkey'];
 
+/* ---------- Functions ---------- */
+function displayAnimals() {
+    var animal = $(this).attr("data-name");
+    var queryURL = "https://www.giphy.com/?t=" + animal + "&apikey=p6ZQy2Z694sls6dGCsQcONAUIiXv8IKA";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var animalDiv = $("<div class='movie'>");
+        var rating = response.Rated;
+        var
+    })
+}
+
 /* ---------- Process ---------- */
