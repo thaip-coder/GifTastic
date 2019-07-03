97dec8018e846a094cee3556fd649d623795eb6b
Thai Phan <thaiphan90@yahoo.com>
2019-07-03 13:59:38 -0500

    loaded 1 gif

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,9 +1,9 @@
 /* ---------- Variables ---------- */
 var animals = ['horse', 'dog', 'cat', 'wolf', 'giraffe', 'whale', 'tiger', 'elephant', 'rabbit', 'monkey'];
-var queryTerm = "";
+var animal = "";
 
 /* ---------- Functions ---------- */
-function createBtn() {
+function createBtns() {
     for (var i = 0; i < animals.length; i++) {
         document.getElementById("animal-buttons").innerHTML += "<button class='animalStyle'>" + animals[i] + "</button>";
     };
@@ -11,18 +11,29 @@ function createBtn() {
 
 function displayAnimals() {
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=p6ZQy2Z694sls6dGCsQcONAUIiXv8IKA&q=" + animals;
+    //var animal = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=p6ZQy2Z694sls6dGCsQcONAUIiXv8IKA&tag=horse";
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {});
+    }).then(function(response) {
+        var gif = response.data.image_original_url;
+        var animalGif = $("<img>");
+        animalGif.attr("src", gif);
+        animalGif.attr("alt", "animal image");
+        $('#gif-section').prepend(animalGif);
+    });
 };
 
 /* ---------- Process ---------- */
 $(document).ready(function() {
 
-    createBtn();
+    createBtns();
+
+    $('#animal-buttons').on('click', function() {
+        displayAnimals();
+    });
 
     $('#add-animal').on('click', function() {
         queryTerm = $('#animal-input').val().trim();
